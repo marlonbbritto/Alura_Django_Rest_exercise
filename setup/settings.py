43df0eea8ff79ad46f0772f55db320f7fc84890e
marlonbbritto <marlonbbritto>
2024-03-05 22:31:49 -0300

    adicionando o cors para que o front react possa consumir a api

--- setup/settings.py
+++ setup/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'escola',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,15 +129,19 @@ REST_FRAMEWORK = {
 #      'rest_framework.permissions.IsAuthenticated',
 #      'rest_framework.permissions.DjangoModelPermissions',
 #  ],
- 'DEFAULT_AUTHENTICATION_CLASSES':[
-     'rest_framework.authentication.BasicAuthentication'
- ],
+#  'DEFAULT_AUTHENTICATION_CLASSES':[
+#      'rest_framework.authentication.BasicAuthentication'
+#  ],
  'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '50/day',
+        'anon': '100/day',
         
     }
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    
+]
